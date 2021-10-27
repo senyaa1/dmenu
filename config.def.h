d409cb9c851bc6240d9d90f8c5c6cb27df83cd6f
Areniy <zloykoteyka145@gmail.com>
2021-10-27 18:22:23 +0300

    Configuration

--- config.def.h
+++ config.def.h
@@ -9,15 +9,14 @@ static int min_width = 500;                    /* minimum width when centered */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 
 /* Size of the window border */
-static const unsigned int border_width = 5;
+static const unsigned int border_width = 2;
 
 static const unsigned int alpha = 0xf0;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 
-static char font[] = "monospace:size=10";
+static char font[] = "Liberation Mono:pixelsize=5:antialias=true";
 static const char *fonts[] = {
 	font,
-	"monospace:size=10",
 };
 
 static char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
@@ -51,10 +50,10 @@ static const unsigned int alphas[SchemeLast][2] = {
 };
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 15;
 /* -h option; minimum height of a menu line */
 static unsigned int lineheight = 0;
-static unsigned int min_lineheight = 8;
+static unsigned int min_lineheight = 14;
 
 /*
  * Characters not considered part of a word while deleting words
